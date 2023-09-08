d42b7d89d46c9e76dc5cfd31b159f427e8a71930
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-09-08 16:14:47 +0100

    add form css

--- src/controllers/form.js
+++ src/controllers/form.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../css/form.css';
 
 export default function Form({ defaultWord, onSearch }) {
 	let [keyword, setKeyword] = useState(defaultWord);
+	let apiKey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
 
 	function handleResponse(res) {
 		onSearch(res.data);
@@ -10,7 +12,6 @@ export default function Form({ defaultWord, onSearch }) {
 
 	useState(
 		() => {
-			let apiKey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
 			let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${defaultWord}&key=${apiKey}`;
 			axios.get(apiUrl).then(handleResponse);
 		},
@@ -19,7 +20,6 @@ export default function Form({ defaultWord, onSearch }) {
 
 	function search(event) {
 		event.preventDefault();
-		let apiKey = '5d4be4co359dcfb3b02ea04bt4fdc01e';
 		let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 		axios.get(apiUrl).then(handleResponse);
 	}
@@ -30,13 +30,18 @@ export default function Form({ defaultWord, onSearch }) {
 
 	return (
 		<div className='Form'>
-			<form onSubmit={search}>
+			<form onSubmit={search} className='input-group mb-3 row m-auto'>
 				<input
 					type='search'
 					placeholder='Search for a word...'
 					onChange={handleKeyword}
+					class='btn btn-secondary col-8'
 				/>
-				<input type='submit' value='Search' autoFocus={true} />
+				<input
+					type='submit'
+					value='Search'
+					class='btn btn-outline-secondary col-4'
+				/>
 			</form>
 		</div>
 	);
