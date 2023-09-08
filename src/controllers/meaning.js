fa0f37c7326ac8dc5e422afd6cdc44466ae446c9
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-09-08 17:25:50 +0100

    add meanings css

--- src/controllers/meaning.js
+++ src/controllers/meaning.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Synonyms from './synonyms';
+import '../css/meanings.css';
 
 export default function Meanings(props) {
 	return (
 		<div className='Meanings'>
 			<h3>{props.meaning.partOfSpeech}</h3>
-
-			<p>{props.meaning.definition}</p>
-			<p>{props.meaning.example}</p>
+			<p className='definition'> {props.meaning.definition}</p>
+			<p>
+				<small>{props.meaning.example}</small>
+			</p>
 			<Synonyms synonyms={props.meaning.synonyms} />
 		</div>
 	);
