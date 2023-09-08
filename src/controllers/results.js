a4115624bb7e7d759f644f9b06e2dc2e5e0433b3
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-09-08 16:51:17 +0100

    add results css

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Meanings from './meaning';
+import '../css/results.css';
 
 export default function Results(props) {
 	console.log(props.results);
@@ -7,8 +8,12 @@ export default function Results(props) {
 	if (props.results) {
 		return (
 			<div className='Results'>
-				<h2>{props.results.word}</h2>
+				<h2>Word</h2>
+				<h1>{props.results.word}</h1>
+				<hr />
+				<h2>Phonetic</h2>
 				<p>/{props.results.phonetic}/</p>
+				<h2>Definition</h2>
 				{props.results.meanings.map(function (meaning, index) {
 					return (
 						<div key={index}>
