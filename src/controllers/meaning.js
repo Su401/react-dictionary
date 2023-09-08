81367bbcf26fa76bf610dad6d5133f17eac8421b
Susana Filipa Ribeiro da Silva <sfrs.iefp@alunos.ipb.pt>
2023-09-08 14:16:12 +0100

    add phonetics

--- src/controllers/meaning.js
+++ src/controllers/meaning.js
@@ -4,9 +4,8 @@ import Synonyms from './synonyms';
 export default function Meanings(props) {
 	return (
 		<div className='Meanings'>
-			<h3>
-				<storng>{props.meaning.partOfSpeech}</storng>
-			</h3>
+			<h3>{props.meaning.partOfSpeech}</h3>
+
 			<p>{props.meaning.definition}</p>
 			<p>{props.meaning.example}</p>
 			<Synonyms synonyms={props.meaning.synonyms} />
