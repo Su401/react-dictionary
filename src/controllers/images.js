f820311a70998a0032f6373cca053770e984335a
Susana Silva <fluffyunicorn.yeah@gmail.com>
2023-12-12 13:57:27 +0000

    improve synonyms

--- src/controllers/images.js
+++ src/controllers/images.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import '../css/images.css';
 
-export default function Images(props) {
-	console.log(props.images);
-	if (props.images) {
-		return (
-			<div className='Images'>
-				<div className='row '>
-					{props.images.map(function (img, i) {
-						return (
-							<div className='col-4'>
-								<a href={img.src.original}>
-									<img
-										className='img-sluid'
-										src={img.src.original}
-										alt={img.alt}
-										key={i}
-									/>
-								</a>
-							</div>
-						);
-					})}
-				</div>
+export default function Images({ images }) {
+	return images ? (
+		<div className='Images'>
+			<div className='row'>
+				{images.map((img, i) => (
+					<div className='col-4' key={i}>
+						<a href={img.src.original}>
+							<img
+								className='img-fluid'
+								src={img.src.original}
+								alt={img.alt}
+							/>
+						</a>
+					</div>
+				))}
 			</div>
-		);
-	} else {
-		return null;
-	}
+		</div>
+	) : null;
 }
