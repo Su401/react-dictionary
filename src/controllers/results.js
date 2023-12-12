e16cdd7eec20fcd936784dfc5b0214a66db27397
Susana Silva <fluffyunicorn.yeah@gmail.com>
2023-12-12 13:54:50 +0000

    fix results null eror

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -2,26 +2,30 @@ import React from 'react';
 import Meanings from './meaning';
 import '../css/results.css';
 
-export default function Results(props) {
-	if (props.results) {
-		return (
-			<div className='Results'>
-				<h2>Word</h2>
-				<h1>{props.results.word}</h1>
-				<hr />
-				<h2>Phonetic</h2>
-				<p>/{props.results.phonetic}/</p>
-				<h2>Definition</h2>
-				{props.results.meanings.map(function (meaning, index) {
-					return (
-						<div key={index}>
-							<Meanings meaning={meaning} />
-						</div>
-					);
-				})}
-			</div>
-		);
-	} else {
+export default function Results({ results }) {
+	if (!results || !results.meanings || results.meanings.length === 0) {
 		return null;
 	}
+
+	return results ? (
+		<div className='Results'>
+			<section>
+				<h2>Word</h2>
+				<h1>{results.word}</h1>
+			</section>
+			<hr />
+			<section>
+				<h2>Phonetic</h2>
+				<p>/{results.phonetic}/</p>
+			</section>
+			<section>
+				<h2>Definition</h2>
+				{results.meanings.map((meaning, index) => (
+					<div key={index}>
+						<Meanings meaning={meaning} />
+					</div>
+				))}
+			</section>
+		</div>
+	) : null;
 }
